d0b750c545599fb564b0ef9a052125f7b8f169c9
bqm3 <bqminh30@gmail.com>
2025-03-12 09:53:39 +0700

    buoi 1 - 11/03

---
+++ README.md
@@ -0,0 +1,19 @@
+I. Javascript
+Nguyên tắc trong js
+
+begin
+arr = [10,4,5,6,0]
+end
+[0,4,5,6,10]
+
+B1
+mình ktra arr.length = 0 return []
+
+B2
+đặt 1 biến: làm cờ
+đặt x = arr[0] = 10
+arr[i] > x => vị trí của x phải đổi sang vị trí thứ i
+
+for(int i = 0; i < arr.length; i++) [10,4,5,6,0]
+
+COMPOENT trong reactjs ///
---
+++ main.js
@@ -0,0 +1,66 @@
+// ghi chú
+// biến; control + /
+// console.log("Hello world! white"); // màu trắng
+// console.warn("Hello world! yellow"); // màu vàng
+// console.error("Hello world! red"); // màu đỏ
+// hiển thị cái em cần xem
+
+// chạy code dùng lệnh ở terminal
+// dùng code runner
+
+// biến
+//  var let const
+// var không bao giờ dùng thằng này , ảnh hưởng đến code (100% k dùng)
+// let const phải dùng
+// C++
+
+// var name = "Minh"; // ghi đè biến trùng nhau dc
+// var name = "Quang";
+
+// console.log("var ", name);
+
+// let name = "Minh"; // không thể ghi đè biến trùng nhau
+// let name = "Quang";
+
+// const name = "Minh"; // không thể ghi đè biến trùng nhau
+// const name = "Quang";
+
+// khác nhau thứ 1:
+// ghi đè và duy nhất
+//
+// phạm vi hoạt động của các biến var: bất cứ đâu
+// phạm vi hoạt động của các biến let: bất cứ đâu
+// phạm vi hoạt động của các biến const: x
+
+// hằng số là 1 biến nó k thể thay đổi
+// const PI= 3.14
+// var name;
+// let name;
+const name = "Minh";
+function changeName() {
+  // xử lý nghiệp vụ
+  console.log("My name is " + name);
+}
+
+function changeName2() {
+  // xử lý nghiệp vụ
+  console.log("My name is " + name);
+}
+
+// gọi hàm để xử lý
+changeName();
+changeName2();
+
+// mã key: db: dbJS mk : 123123ab  => const
+// let: nhập họ tên => chỉnh sửa let => username: Bùi Quang Minh => Bùi Minh Quang
+// tuyệt đối k dc dùng VAR
+
+// đẩy code lên git quản lý source code  => dạy em thêm merge + permission + ...
+// html css
+// js biến, phạm vi hđ của nó =>  hàm + data types + công thức + điều kiện + logic
+
+// 13b js + html + css + git + jquery + fetch axios
+// project mini html css js
+// nodejs: 10 buổi
+// super project html css js nodejs + fetch axios ( API )
+// php: cơ bản =3-4 1 dùng framework + php/java/golang/python
